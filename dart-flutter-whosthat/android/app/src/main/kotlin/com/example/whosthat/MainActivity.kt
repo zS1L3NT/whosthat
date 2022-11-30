0b0aa896e1fc2866d9661863dabc9232a296bd1b
Zechariah Tan <zechariahtan144@gmail.com>
2022-11-30 13:28:43 +0800

    ✨ initialize flutter project

---
+++ dart-flutter-whosthat/android/app/src/main/kotlin/com/example/whosthat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zectan.whosthat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dart-flutter-whosthat/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:whosthat/app.dart';
+
+void main() {
+  runApp(const App());
+}

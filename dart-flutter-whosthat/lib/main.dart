afe35b59313994cf39c80503e024e5f67a0fab65
Zechariah <zechariahtan144@gmail.com>
2022-12-06 00:29:02 +0800

    🚧 working on flutter app, need frontend to be done first
    Patched to remove AWS keys

--- dart-flutter-whosthat/lib/main.dart
+++ dart-flutter-whosthat/lib/main.dart
@@ -1,6 +1,20 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
+import 'package:aws_iot_data_api/iot-data-2015-05-28.dart';
 import 'package:flutter/material.dart';
 import 'package:whosthat/app.dart';
 
-void main() {
-  runApp(const App());
+void main() async {
+  runApp(
+    App(
+      iot: IoTDataPlane(
+        region: "",
+        endpointUrl: "",
+        credentials: AwsClientCredentials(
+          accessKey: "",
+          secretKey: "",
+        ),
+      ),
+    ),
+  );
 }
